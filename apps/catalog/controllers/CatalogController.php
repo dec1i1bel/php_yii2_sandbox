5548325a8a269a1e2e7dc72b0927fb1fde0bb93f
vaba2660 <v.balabanov85@gmail.com>
2020-01-04 16:19:38 +0300

    .

--- apps/catalog/controllers/CatalogController.php
+++ apps/catalog/controllers/CatalogController.php
@@ -10,9 +10,20 @@ class CatalogController extends Controller
   public function actionIndex()
   {
     $query = Catalog::find();
+
     $pagination = new Pagination([
       'defaultPageSize' => 6,
       'totalCount' => $query->count(),
+    ]);
+
+    $items = $query->orderBy('name')
+                  ->offset($pagination->offset)
+                  ->limit($pagination->limit)
+                  ->all();
+
+    return $this->render('index', [
+      'items' => $items,
+      'pagination' => $pagination,
     ]);
   }
 }
