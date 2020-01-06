445cf129e43e5dadc46fa03a0d6f2108b71747f3
vaba2660 <v.balabanov85@gmail.com>
2020-01-06 19:08:27 +0300

    .\

--- apps/catalog/views/catalog/_search.php
+++ apps/catalog/views/catalog/_search.php
@@ -27,7 +27,6 @@ use yii\widgets\ActiveForm;
 
     <div class="form-group">
         <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
-        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
